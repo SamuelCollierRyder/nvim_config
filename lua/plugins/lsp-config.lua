91905d05ff9e47810b937a7a3aff9e648044a6ef
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2023-12-30 14:05:05 +0100

    Added new git config

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,29 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup()
+        end
+    },
+
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            lspconfig.pyright.setup({})
+            lspconfig.tsserver.setup({})
+            lspconfig.emmet_language_server.setup({})
+            vim.keymap.set('n', '<leader>gh', vim.lsp.buf.hover)
+            vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
+            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+        end
+    }
+}
