aed900b2f14fc566119c2f15dcc1301faef0f403
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2024-01-01 19:31:30 +0100

    Small update

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,12 +1,15 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    config = function()
-        vim.keymap.set('n', '<leader>f', ':Neotree filesystem reveal bottom<CR>')
-    end
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    vim.keymap.set('n', '<leader>f', ':Neotree filesystem reveal bottom<CR>')
+    require("neo-tree").setup({
+      close_if_last_window = true
+    })
+  end
 }
