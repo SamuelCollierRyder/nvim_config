ea98f578613184a4b6a8d6889d7af3146b2045ad
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2024-01-01 13:31:16 +0100

    Renamed and rewrote cmp.lua

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,11 +16,12 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.pyright.setup({})
-            lspconfig.tsserver.setup({})
-            lspconfig.emmet_language_server.setup({})
+            lspconfig.lua_ls.setup({capabilities = capabilities})
+            lspconfig.pyright.setup({capabilities = capabilities})
+            lspconfig.tsserver.setup({capabilities = capabilities})
+            lspconfig.emmet_language_server.setup({capabilities = capabilities})
             vim.keymap.set('n', '<leader>gh', vim.lsp.buf.hover)
             vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
             vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
