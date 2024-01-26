38eaaab492e4c6590ecde1d5c8787127f7dff5c1
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2024-01-26 22:13:17 +0100

    Added scrollbar

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,4 +6,3 @@ vim.cmd("set relativenumber")
 vim.g.mapleader = " "
 vim.keymap.set({"n", "v"}, "<leader>p", "\"+p") -- paste from clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", "\"+y") -- yank to clipboard
---vim.keymap.set({"n", "i", "v"},"<F5>",":split<CR>:terminal python3 %<CR>")
