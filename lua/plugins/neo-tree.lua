9e5e21651d67ca385a37e78fdb096fbc6d9e00d7
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2024-03-16 23:21:37 +0800

    Added trouble and made changes to neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,15 +1,20 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
+
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+
+
   config = function()
-    vim.keymap.set('n', '<leader>f', ':Neotree filesystem reveal left<CR>')
+    vim.keymap.set('n', '<leader>f', ':Neotree toggle<CR>')
     require("neo-tree").setup({
       close_if_last_window = true,
+      auto_clean_after_session_restore = true,
       window = {
         position = "left",
         width = 30,
