06b1def4b9c6c4424b137183d20e694c3ab15a7f
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2024-03-21 10:47:22 +0800

    Improved neo-tree config with icons

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -12,12 +12,26 @@ return {
 
   config = function()
     vim.keymap.set("n", "<leader>f", ":Neotree toggle<CR>")
+      vim.fn.sign_define("DiagnosticSignError",
+        {text = " ", texthl = "DiagnosticSignError"})
+      vim.fn.sign_define("DiagnosticSignWarn",
+        {text = " ", texthl = "DiagnosticSignWarn"})
+      vim.fn.sign_define("DiagnosticSignInfo",
+        {text = " ", texthl = "DiagnosticSignInfo"})
+      vim.fn.sign_define("DiagnosticSignHint",
+        {text = "󰌵", texthl = "DiagnosticSignHint"})
+
     require("neo-tree").setup({
       close_if_last_window = true,
       window = {
         position = "left",
         width = 30,
       },
+        default_component_configs = {
+          modified = {
+            symbol = "C",
+          }
+        }
     })
   end
 }
